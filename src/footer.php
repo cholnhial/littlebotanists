e8d241cf81f5947ede1d1fac766b6ccf3824331e
suhudev <suhudev@gmail.com>
2021-09-17 13:47:56 +1000

    Created a page for getting the name of the user.

---
+++ src/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer mt-auto py-3 bg-light">
+    <div class="container">
+        <span class="text-muted">Powered by Boostrap 5</span>
+    </div>
+</footer>
+
+<script src="js/bootstrap.bundle.min.js"></script>
