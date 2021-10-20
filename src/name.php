91e76f6d1dc1e2c504ef16528d86ccbcae69663c
Chol Nhial <chol.nhial.chol@gmail.com>
2021-10-20 11:01:36 +1000

    changes

--- src/name.php
+++ src/name.php
@@ -1,25 +1,41 @@
+<style>
+    body {
+        background-image: url("/img/nameinput.png");
+        height: 100%;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    .text-size {
+        font-size: 1.5rem;
+    }
+</style>
 <div class="position-relative" id="name-card-wrapper">
     <div class="position-absolute name-card-top start-50 translate-middle">
-        <div class="card text-end" style="width: 25rem;">
-            <div class="card-header text-center">
+        <div class="card transparent-panel text-end" style="width: 50rem;">
+            <div class="card-header text-center text-size">
                 Provide your username
             </div>
             <div class="card-body">
-                <h5 class="card-title text-center">Almost there!</h5>
-                <p class="card-text text-center">In order personalise the experience for you please provide us with your username.</p>
+                <div class="d-flex justify-content-center">
+                    <img class="namelogo mx-auto" src="img/Logo.png" alt="logo"/>
+                </div>
+                <h5 class="card-title text-center text-size">Almost there!</h5>
+                <p class="card-text text-center text-size">In order personalise the experience for you please provide us with your username.</p>
 
                 <form id="name-form">
                     <div class="mb-3">
-                        <label class="form-label" for="name">Your Nickname</label>
+                        <label class="form-label text-size" for="name">Your Nickname</label>
                         <input id="name" class="form-control form-control-lg" type="text" placeholder="Alex101">
-                        <div id="validateNameInValidFeedback" class="d-none invalid-feedback">
+                        <div id="validateNameInValidFeedback" class="d-none invalid-feedback text-size">
                         </div>
-                        <div id="validateNameValidFeedback" class="d-none valid-feedback">
+                        <div id="validateNameValidFeedback" class="d-none valid-feedback text-size">
                             Looks good!
                         </div>
                     </div>
 
-                    <button type="submit" id="submit-btn" disabled class="btn btn-outline-primary friendly-btn">START</button>
+                    <button type="submit" id="submit-btn" disabled class="btn btn-outline-primary friendly-btn text-size">START</button>
                 </form>
             </div>
         </div>
--- src/study.php
+++ src/study.php
@@ -1,46 +1,75 @@
-<div class="d-flex justify-content-center mt-5">
+<style>
+    body {
+        background-image: url("/img/module.png");
+        height: 100%;
+        background-position-y: 3rem;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    .choose-cat {
+        color: var(--lbsecondary);
+        font-weight: bold;
+        margin-top: 5rem;
+        margin-left: 5%;
+    }
+
+    .transparent-panel {
+        border: 7px solid white;
+        border-radius: 10px;
+        background-color: rgba(255, 255, 255, 0.5);
+        padding: 0 !important;
+    }
+
+    .card-body {
+        padding: 0.4rem !important;
+    }
+
+</style>
+<h1 id="categories" class="choose-cat animate__animated animate__slideInLeft animate__slow">Choose a study category</h1>
+<div class="d-flex justify-content-center" style="margin-top: 9rem">
     <div class="row row-cols-1 w-50">
         <div class="col mb-2">
             <a class="text-decoration-none link-dark" href="/index.php?cat=study_categories&plantCategoryType=Groundcovers&module=Groundcovers">
-                <div class="card menu-card">
+                <div class="card menu-card transparent-panel">
                     <div class="card-body text-center">
-                        <h2>Groundcovers <span class="badge bg-success">Quizzed</span></h2>
+                        <h4>Groundcovers <span class="badge bg-success">Quizzed</span></h4>
                     </div>
                 </div>
             </a>
         </div>
         <div class="col mb-2">
             <a class="text-decoration-none link-dark" href="/index.php?cat=study_categories&plantCategoryType=Tufting plants and grasses&module=Tufting Plants">
-            <div class="card menu-card">
+            <div class="card menu-card transparent-panel">
                 <div class="card-body text-center">
-                    <h2>Tufting Plants</h2>
+                    <h4>Tufting Plants</h4>
                 </div>
             </div>
             </a>
         </div>
         <div class="col mb-2">
             <a class="text-decoration-none link-dark"  href="/index.php?cat=study_categories&plantCategoryType=Climbers&module=Small Climbers">
-                <div class="card menu-card">
+                <div class="card menu-card transparent-panel">
                     <div class="card-body text-center">
-                        <h2>Small Climbers</h2>
+                        <h4>Small Climbers</h4>
                     </div>
                 </div>
             </a>
         </div>
         <div class="col mb-2">
             <a class="text-decoration-none link-dark"  href="/index.php?cat=study_categories&plantCategoryType=Medium shrubs - three to five metres&module=Medium Shrubs">
-                <div class="card menu-card">
+                <div class="card menu-card transparent-panel">
                     <div class="card-body text-center">
-                        <h2>Medium Shrubs</h2>
+                        <h4>Medium Shrubs</h4>
                     </div>
                 </div>
             </a>
         </div>
         <div class="col">
             <a class="text-decoration-none link-dark"  href="/index.php?cat=study_categories&plantCategoryType=Shade and feature trees - over five metres&module=Shade & Feature Trees">
-                <div class="card menu-card">
+                <div class="card menu-card transparent-panel">
                     <div class="card-body text-center">
-                        <h2>Shade & Feature Trees</h2>
+                        <h4>Shade & Feature Trees</h4>
                     </div>
                 </div>
             </a>
